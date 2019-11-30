c45e2d73ae8c1f62fdbf4db9f4665b9b8ec94afc
NatyDev <natydev@aol.com>
2019-11-30 14:49:42 +0100

    Improve webpacker configuration

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,14 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import 'bootstrap/dist/js/bootstrap';
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("bootstrap/dist/js/bootstrap")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -14,16 +17,12 @@
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import './src/application.scss'
 
-import Rails from 'rails-ujs'
-Rails.start()
-
-import $ from 'jquery'
-
-// NOTE: bootstrap requires Popper.js
-import 'bootstrap'
-import '@fortawesome/fontawesome-free/js/all'
-
-$(document).ready(function () {
-  // insert here some init
-})
+// import '@fortawesome/fontawesome-free/js/all'
+// 
+// import $ from 'jquery'
+// 
+// $(document).ready(function () {
+//   // insert here some init
+// })
