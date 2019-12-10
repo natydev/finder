fe9477634a874e7649d70f8422bed36eed1bd49c
NatyDev <natydev@aol.com>
2019-12-10 08:00:51 +0100

    feat: Add decorator  modules for picture and paginating

---
+++ app/decorators/picture_deco.rb
@@ -0,0 +1,19 @@
+module PictureDeco
+  extend ActiveSupport::Concern
+
+  def image_tag_medium
+    image_tag_picture(:medium)
+  end
+
+  def image_tag_small
+    image_tag_picture(:small)
+  end
+
+  private
+
+  def image_tag_picture(size)
+    h.image_tag(object.picture(size).url, class: 'img-fluid') if object.picture.present?
+  rescue NoMethodError
+    Rails.logger.error "Picture version not present for #{object.class} #{object.id}"
+  end
+end
