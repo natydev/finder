fc1b203f5af47dc41ce4933e60bdeb1af0665d09
NatyDev <natydev@aol.com>
2019-12-27 11:56:55 +0100

    feat: Improve search with list and initial (empty) dashboard

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,12 +3,19 @@ class SearchesController < ApplicationController
 
   def index
     @q = Box.ransack(params[:q])
-    @boxes = @q.result(distinct: true).includes(:spot, items: :tags)
-    .left_joins(:items)
-    .select(
-      'boxes.id, boxes.summary, boxes.spot_id, boxes.code, boxes.typology, boxes.updated_at,
-       items.box_id, items.id AS item_id, items.summary AS item_summary, items.updated_at AS item_updated_at'
-    ).page(params[:page])
+    @boxes = if @q.conditions.present?
+      @display = true
+      @q.result(distinct: true)
+      .includes(:spot, items: :tags)
+      .left_joins(:items)
+      .where(owner_id: current_user.id)
+      .select(
+        'boxes.id, boxes.summary, boxes.spot_id, boxes.code, boxes.typology, boxes.updated_at,
+         items.box_id, items.id AS item_id, items.summary AS item_summary, items.updated_at AS item_updated_at'
+      ).page(params[:page])
+    else
+      @display = false
+    end
   end
 
   private
