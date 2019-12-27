38969d033c4cf4a729acfc27dd834a4278526a67
NatyDev <natydev@aol.com>
2019-12-27 11:56:14 +0100

    fix: add #elaborate_picture to ItemOp::Create

--- app/operations/item_op/create.rb
+++ app/operations/item_op/create.rb
@@ -1,5 +1,12 @@
 module ItemOp
   class Create < BaseCreator
+    include ElaboratePicture
     option :model_klass, default: proc { Item }
+
+    def call
+      prepare_record
+      .bind(method(:elaborate_picture))
+      .bind(method(:persist_record))
+    end
   end
 end
