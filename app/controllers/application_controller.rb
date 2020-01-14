1ad2f70cc5c923a147c79c697aa1ef36bf8d1b35
NatyDev <natydev@aol.com>
2020-01-14 22:10:08 +0100

    feat: Add browser language detection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   include CRUDHelper
+  before_action :set_locale_based_on_browser
   before_action :context_icon
 
   private
@@ -10,5 +11,29 @@ class ApplicationController < ActionController::Base
     # a contextual icon class
     def context_icon
       @context_icon = nil
+    end
+
+    def set_locale_based_on_browser
+      locale = extract_locale_from_accept_language_header
+
+      I18n.locale =
+        if locale_valid?(locale)
+          locale
+        else
+          I18n.default_locale
+        end
+    end
+
+    private
+
+    def locale_valid?(locale)
+      I18n.available_locales.map(&:to_s).include?(locale)
+    end
+
+    def extract_locale_from_accept_language_header
+      accept_language = request.env['HTTP_ACCEPT_LANGUAGE']
+      return unless accept_language
+
+      accept_language.scan(/^[a-z]{2}/).first
     end
 end
