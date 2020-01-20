55abd427de715dd606c33e8ca2c048139828db2c
NatyDev <natydev@aol.com>
2020-01-20 21:01:20 +0100

    fix: Add popper.js and fix jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,11 +6,12 @@ import 'bootstrap4c-chosen/dist/css/component-chosen';
 import 'cocoon-js';
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import 'jquery/dist/jquery.slim'
+import 'popper.js/dist/esm/popper'
 
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
-require("jquery");
 require("bootstrap/dist/js/bootstrap");
 
 
@@ -27,7 +28,38 @@ import '@fortawesome/fontawesome-free/js/all';
 // 
 // import $ from 'jquery'
 // 
-$(document).ready(function () {
-//   // insert here some init
-    //$('.chosen-select').chosen();
+$(document).on("turbolinks:load", function () {
+
+
+  $('.form-control-chosen').chosen();
+  $('[data-toggle="tooltip"]').tooltip();
+
+  var $fieldQuantity = $('#field_quantity');
+  var $fieldFreeRatio = $('#field_free_ratio');
+  var $fieldTags = $('#field_tags');
+
+  // box form:
+  var $freeRatio = $('#box_free_ratio');
+  $freeRatio.css('--val', $freeRatio.val());
+  $freeRatio.on('change', function() {
+    $(this).css('--val', $(this).val());
+  });
+
+  if ($('#box_typology_cl').is(":checked")) {
+    $fieldQuantity.hide();
+    $fieldTags.hide();
+  }
+  if ($('#box_typology_st').is(":checked")) {
+    $fieldFreeRatio.hide();
+  }
+  $('#box_typology_cl').on('click', function() {
+    $fieldQuantity.hide();
+    $fieldTags.hide();
+    $fieldFreeRatio.show();
+  });
+  $('#box_typology_st').on('click', function() {
+    $fieldQuantity.show();
+    $fieldTags.show();
+    $fieldFreeRatio.hide();
+  });
 })
