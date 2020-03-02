7be26f58463a5aef4eafa5fe63b6204730ed6c2a
NatyDev <natydev@aol.com>
2020-03-02 17:17:03 +0100

    fix: Correct query for stats index

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -7,15 +7,16 @@ class StatsController < ApplicationController
     table = PaperTrail::Version.arel_table
     @latest_updates = VersionDecorator.decorate_collection(
       PaperTrail::Version.includes(:item)
-        .select('DISTINCT ON (item_id) item_id, item_type, created_at')
-        .where(
-          table[:item_type].eq('Box').or(table[:item_type].eq('Item'))
-        )
-        .where(
-          table[:event].eq('update')
-        )
-        .limit(20).order(table[:item_id]).to_a
-        .sort_by(&:created_at).reverse
+      .select('item_id, item_type, created_at').from(
+        PaperTrail::Version
+          .select('DISTINCT ON (item_id) item_id, item_type, created_at')
+          .where(
+            table[:item_type].eq('Box').or(table[:item_type].eq('Item'))
+          )
+          .where(
+            table[:event].eq('update')
+          )
+      ).order('created_at desc').limit(15)
     )
   end
 
